33299617eab7e899837b2ab16434119c0e0a1442
agrup <agustinrodriguez206@gmail.com>
2019-10-15 23:57:03 -0300

    fix edges count

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import networkx as nx
 import random
 import datetime
 import csv
+
 #config 
 folder = 'graph_gml/'
 csv_file =''
@@ -15,16 +16,14 @@ def graph_add_random_weight(G,ini=1,end=10):
         G[o][d]['weight'] = random.randint(ini,end)
     return G
 
-def create_er_graph(nodos=[10,100,1000],probs=[0.8,0.5,0.1]):
+def create_er_graph(nodos=[10,100,1000],edges=[10,100,1000]):
     graphs_er=[]
     for cnt_nodos in nodos:
-        for p in probs:
-            graph = nx.gnm_random_graph(cnt_nodos,(cnt_nodos*cnt_nodos)*p)
+        for cnt_edges in edges:
+            graph = nx.gnm_random_graph(cnt_nodos,cnt_edges)
             graph = graph_add_random_weight(graph)
             graphs_er.append(graph)
-            print("{}erdos_renyi_nodos={}_prob={}.edges.gml".format(folder,str(cnt_nodos),str(p)))
-            #nx.write_edgelist(graph,"{}erdos_renyi_nodos={}_prob={}.edges.gml".format(folder,str(cnt_nodos),str(p)))
-            #nx.write_gml(graph,"{}erdos_renyi_nodos={}_prob={}.nodes.gml".format(folder,str(cnt_nodos),str(p)))
+            print("erdos_renyi_nodos={}_prob={}.edges.gml".format(str(cnt_nodos),str(cnt_edges)))
     return graphs_er
 
 def create_ba_graph(nodos=[50,100,1000],probs=[1,0.5]):
@@ -36,25 +35,20 @@ def create_ba_graph(nodos=[50,100,1000],probs=[1,0.5]):
                 graph = nx.barabasi_albert_graph(cnt_nodos,int(cnt_nodos*p)-1)
             else:
                 graph = nx.barabasi_albert_graph(cnt_nodos,int(cnt_nodos*p))
-            #print("barabasi edges",int(cnt_nodos*p),cnt_nodos,p,"nodos",cnt_nodos)
             graph_add_random_weight(graph)
             graphs_ba.append(graph)
-            print("{}barabasi_albert_nodos={}_probs={}".format(folder,str(cnt_nodos),str(p)))
-            #nx.write_edgelist(graph,"{}barabasi_albert_nodos={}_probs={}.edges.gml".format(folder,str(cnt_nodos),str(p)))
-            #nx.write_gml(graph,"{}barabasi_albert_nodos={}_probs={}.nodes.gml".format(folder,str(cnt_nodos),str(p)))
+            print("barabasi_albert_nodos={}_probs={}".format(str(cnt_nodos),str(p)))
     return graphs_ba
 
 
 def get_floyd_washall_time(G,weight='weight'):
     begin = datetime.datetime.now()
-    #nx.floyd_warshall(G,weight=weight)
     _distance = nx.floyd_warshall(G,weight=weight)
     end=datetime.datetime.now()
     return(end-begin)
 
 def get_johnson_time(G,weight='weight'):
     begin = datetime.datetime.now()
-    #nx.johnson(G,weight=weight)
     _distance = nx.johnson(G,weight=weight)
     end=datetime.datetime.now()
     return(end-begin)
@@ -75,11 +69,20 @@ with open('result.csv','w')as file:
 
 # creo los grafos con 1000 nodos
 nodos=10**3
+edges=[nodos*nodos,nodos,nodos/2]
 
-er = create_er_graph(nodos=[nodos])
+er = create_er_graph(nodos=[nodos],edges=edges)
 print("er created")
 time_fw = get_floyd_washall_time(er[0])
 time_jo = get_johnson_time(er[0])
+save_results(er,"erdos renyi",time_fw,time_jo)
+
+time_fw = get_floyd_washall_time(er[1])
+time_jo = get_johnson_time(er[1])
+save_results(er,"erdos renyi",time_fw,time_jo)
+
+time_fw = get_floyd_washall_time(er[2])
+time_jo = get_johnson_time(er[2])
 save_results(er,"erdos renyi",time_fw,time_jo)
 
 ba = create_ba_graph(nodos=[nodos])
@@ -88,30 +91,59 @@ time_fw = get_floyd_washall_time(ba[0])
 time_jo = get_johnson_time(ba[0])
 save_results(er,"barabasi albert",time_fw,time_jo)
 
+time_fw = get_floyd_washall_time(ba[1])
+time_jo = get_johnson_time(ba[1])
+save_results(er,"barabasi albert",time_fw,time_jo)
 
 # creo los grafos con 10000 nodos
 nodos=10**4
+edges=[nodos*nodos,nodos,nodos/2]
 
-er = create_er_graph(nodos=[nodos])
+er = create_er_graph(nodos=[nodos],edges=edges)
 print("er created")
 time_fw = get_floyd_washall_time(er[0])
 time_jo = get_johnson_time(er[0])
 save_results(er,"erdos renyi",time_fw,time_jo)
 
-ba = create_ba_graph(nodos=[nodos])
+time_fw = get_floyd_washall_time(er[1])
+time_jo = get_johnson_time(er[1])
+save_results(er,"erdos renyi",time_fw,time_jo)
+
+time_fw = get_floyd_washall_time(er[2])
+time_jo = get_johnson_time(er[2])
+save_results(er,"erdos renyi",time_fw,time_jo)
+
+
+ba = create_ba_graph(nodos=[nodos],)
 print("ba created")
 time_fw = get_floyd_washall_time(ba[0])
 time_jo = get_johnson_time(ba[0])
 save_results(er,"barabasi albert",time_fw,time_jo)
+
+time_fw = get_floyd_washall_time(ba[1])
+time_jo = get_johnson_time(ba[1])
+save_results(er,"barabasi albert",time_fw,time_jo)
+
 
 # creo los grafos con 100000 nodos
 nodos=10**5
+edges=[nodos*nodos,nodos,nodos/2]
 
-er = create_er_graph(nodos=[nodos])
+er = create_er_graph(nodos=[nodos],edges=edges)
 print("er created")
 time_fw = get_floyd_washall_time(er[0])
 time_jo = get_johnson_time(er[0])
 save_results(er,"erdos renyi",time_fw,time_jo)
+
+time_fw = get_floyd_washall_time(er[1])
+time_jo = get_johnson_time(er[1])
+save_results(er,"erdos renyi",time_fw,time_jo)
+
+time_fw = get_floyd_washall_time(er[2])
+time_jo = get_johnson_time(er[2])
+save_results(er,"erdos renyi",time_fw,time_jo)
+
+
 
 ba = create_ba_graph(nodos=[nodos])
 print("ba created")
@@ -119,6 +151,9 @@ time_fw = get_floyd_washall_time(ba[0])
 time_jo = get_johnson_time(ba[0])
 save_results(er,"barabasi albert",time_fw,time_jo)
 
+time_fw = get_floyd_washall_time(ba[1])
+time_jo = get_johnson_time(ba[1])
+save_results(er,"barabasi albert",time_fw,time_jo)
 
 # for graph in list(er):
 #     time_fw,dist = get_floyd_washall_time(graph)
